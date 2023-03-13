71e0fea44e072fbc5fd95ededc76119ef2a20c67
Lennaben <BENALOUILENNA@GMAIL.COM>
2023-03-13 15:29:03 +0100

    html modal

--- FrontEnd/scripts/api/admin.js
+++ FrontEnd/scripts/api/admin.js
@@ -2,10 +2,23 @@
 console.log(localStorage.getItem("token"))
 //Si oui, Affcher Barre d'amdmin noir et les bouton modifier, faire disparer les filtrer (display none)
 
+// essai 1
 // let element = document.querySelector("filters-container");
 // element.style.display = "none";
 
 // let element = document.querySelector("filters-container");
 // element.style.display = "block";
 
-const myArray = [""]
+
+
+_______________________________________
+// essai 2
+// let url = window.login.html;
+
+// if (url.includes ("secret")){
+//     const elementAmasquer = document.getElementById("filters-container");
+
+//     elementAmasquer.classList.toggle("hidden");
+// }
+
+
